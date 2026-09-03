79166cbee5e6029a980bd3523e74b02e0e9181e0
zwj136570160-cpu <zwj136570160@gmail.com>
2026-09-04 00:42:53 +0800

    完成了beginPanel

---
+++ Assets/Scripts/UI/BeginScene/BeginPanel.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+/// <summary>
+/// 开始界面
+/// </summary>
+public class BeginPanel : BasePanel<BeginPanel>
+{
+    public UIButton btnBegin;
+    public UIButton btnRank;
+    public UIButton btnSetting;
+    public UIButton btnQuit;
+
+    public override void Init()
+    {
+        //监听按钮事件
+        btnBegin.onClick.Add(new EventDelegate(() =>
+        {
+            //显示选角面板
+            //隐藏自己
+            HideMe();
+        }));
+        btnRank.onClick.Add(new EventDelegate(() =>
+        {
+            //显示排行榜面板
+        }));
+        btnSetting.onClick.Add(new EventDelegate(() =>
+        {
+            //显示设置面板
+        }));
+        btnQuit.onClick.Add(new EventDelegate(() =>
+        {
+            //退出游戏
+            Application.Quit();
+        }));
+    }
+}
